febb4d8e3f7f8208d5e41d78691b12df18c1979d
todtsui <todtsui@163.com>
2022-08-25 10:15:33 +0800

    Initial commit

---
+++ app/src/main/java/com/kp/android/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,94 @@
+package com.kp.android.nerdlauncher
+
+import android.content.Intent
+import android.content.pm.ResolveInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "NerdLauncherActivity"
+
+class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        //传入0参数,表示不打算修改查询结果
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
+        Log.i(TAG, "Found ${activities.size} activities")
+        recyclerView.adapter = ActivityAdapter(activities)
+    }
+
+    private class ActivityHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        private val nameTextView = itemView as TextView
+        private lateinit var resolveInfo: ResolveInfo
+
+        init {
+            nameTextView.setOnClickListener(this)
+        }
+
+        fun bindActivity(resolveInfo: ResolveInfo) {
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+        }
+
+        override fun onClick(view: View) {
+            val activityInfo = resolveInfo.activityInfo
+            //ACTION_MAIN打开程序的主Activity
+            val intent = Intent(Intent.ACTION_MAIN).apply {
+                setClassName(activityInfo.applicationInfo.packageName, activityInfo.name)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            val context = view.context
+            context.startActivity(intent)
+        }
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) : RecyclerView.Adapter<ActivityHolder>() {
+
+        override fun onCreateViewHolder(container: ViewGroup, viewType: Int): ActivityHolder {
+            //获取一个布局填充器实例
+            val layoutInflater = LayoutInflater.from(container.context)
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, container, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return activities.size
+        }
+    }
+}
